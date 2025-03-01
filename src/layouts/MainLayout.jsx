4cf5e088c2fa68adf09ebbed25771eac95deafab
Vishal Tarwatkar <vishaltarwatkar321@gmail.com>
2025-03-01 16:40:07 +0530

    Smooth Scroll added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import Lenis from "lenis";
 
 const MainLayout = () => {
+   useEffect(() => {
+      const lenis = new Lenis();
+
+      function raf(time) {
+         lenis.raf(time);
+         requestAnimationFrame(raf);
+      }
+
+      requestAnimationFrame(raf);
+   }, []);
+
    return (
       <div>
          <Header />
--- src/pages/home/components/AboutSection.jsx
+++ src/pages/home/components/AboutSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const AboutSection = ({ scrollYProgress }) => {
    const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-   const rotate = useTransform(scrollYProgress, [0, 1], [8, 0]);
+   const rotate = useTransform(scrollYProgress, [0, 1], [5, 0]);
 
    return (
       <motion.div
